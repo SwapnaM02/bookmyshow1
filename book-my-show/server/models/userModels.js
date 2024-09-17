fd2e0fc45debadbe7b6a41b8c95d5c57f03f9fe0
SwapnaM02 <m.v.swapnareddy@gmail.com>
2024-09-17 16:53:02 +0530

    frontend and backend code

---
+++ book-my-show/server/models/userModels.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["admin", "user", "partner"],
+    default: "user",
+  },
+
+ 
+});
+
+const UserModel = mongoose.model("users", userSchema);
+
+module.exports = UserModel;
